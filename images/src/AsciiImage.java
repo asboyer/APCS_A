ff0d10bb5a04663f50dac62145958f170d6b36f5
boyer <asboyer@gmail.com>
2021-05-24 18:24:07 -0400

    finished imgs

--- images/src/AsciiImage.java
+++ images/src/AsciiImage.java
@@ -14,7 +14,7 @@ public class AsciiImage extends CustomImage{
     @Override
     public void draw(Graphics2D g2){
         Pixel[][] pixels = getPixels();
-        int pixelSize = 5;
+        int pixelSize = 4;
         g2.setFont(new Font("Roboto Monaco", Font.PLAIN, 6));
         String ascii = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'.";
         ascii = new StringBuilder(ascii).reverse().toString();
@@ -23,7 +23,15 @@ public class AsciiImage extends CustomImage{
                 Pixel p = pixels[r][c];
                 double a = (p.getBlue() + p.getRed() + p.getGreen())/(3);
                 int color = (int)(ascii.length()*a/255.0) - 1;
-                String string = ascii.substring(color, color + 1);
+                String string;
+                if(color < 0){
+                    string = ascii.substring(0, 1);
+                }
+                else {
+//                    System.out.println(color + " | " + ascii.length());
+                    string = ascii.substring(color, color + 1);
+                }
+
                 g2.drawString(string, c, r);
             }
         }
--- images/src/Main.java
+++ images/src/Main.java
@@ -19,13 +19,17 @@ public class Main extends JPanel {
 //        images.add(new PixelatedImage("slothface.png", 0, 287));
 
         images = new ArrayList<>();
-//        images.add(new AsciiImage("astro.png", 0, 0));
-        images.add(new MirrorImage("astro.png", 500, 0));
-        images.add(new NegativeImage("astro.png", 500, 500));
-        images.add(new SepiaImage("astro.png", 0, 500*2));
-        images.add(new GreyImage("astro.png", 1000, 0));
-        images.add(new FadeWhiteImage("astro.png", 500, 500*2));
-        images.add(new PixelatedImage("astro.png", 0, 500));
+        images.add(new AsciiImage("astro.png", 0, 0));
+        images.add(new AsciiImage("astro.png", 0, 0));
+        images.add(new MirrorImage("astro.png", 250, 0));
+        images.add(new NegativeImage("astro.png", 250, 250));
+        images.add(new SepiaImage("astro.png", 0, 250*2));
+        images.add(new GreyImage("astro.png", 250*2, 0));
+        images.add(new FadeWhiteImage("astro.png", 250, 250*2));
+        images.add(new PixelatedImage("astro.png", 0, 250));
+        images.add(new CustomImage("astro.png", 250*2, 250*2));
+        images.add(new GreenImage("astro.png", 250*2, 250));
+
 
 
     }
@@ -41,7 +45,7 @@ public class Main extends JPanel {
     public static void main(String[] args) {
         JFrame window = new JFrame("Images!");
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        window.setBounds(0, 0, 800, 800 + 22); //(x, y, w, h) 22 due to title bar.
+        window.setBounds(0, 0, 765, 765 + 22); //(x, y, w, h) 22 due to title bar.
 
         Main panel = new Main(800, 800);
 
